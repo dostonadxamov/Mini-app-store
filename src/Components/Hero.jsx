656fec9ab8774529b237de683b17bb5ffa202cf3
doston <dostonadxamov222@gmail.com>
2025-07-20 22:22:57 +0500

    initial commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 export default function Hero() {
   return (
     
-    <div data-aos="flip-right" className="px-[15px] pt-[50px] max-w-[1480px] m-auto bg-gradient-to-r mt-[75px] rounded-[50px]  from-purple-600 to-pink-500 text-white ">
+    <div data-aos="flip-right" className="px-[15px] pb-[20px] pt-[50px] max-w-[1480px] m-auto bg-gradient-to-r mt-[75px] rounded-[50px]  from-purple-600 to-pink-500 text-white ">
       <div className="max-w-6xl  mx-auto flex flex-col md:flex-row items-center justify-between ">
         
       
@@ -23,7 +23,7 @@ export default function Hero() {
         <div className="w-full md:w-1/2">
           <img
             src={logo}
-            alt="Hero Image"
+            alt="Hero image"
             className="w-full rounded-xl transition-transform duration-300 hover:scale-110 "
           />
         </div>
