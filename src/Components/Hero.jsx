39b6a19a85c983f9c73de4cb4dbf1c3c6617b007
dostonadxamov <dostonadxamov222@gmail.com>
2025-08-08 21:54:58 +0500

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 export default function Hero() {
   return (
     
-    <div data-aos="flip-right" className="px-[15px] pb-[20px] pt-[50px] max-w-[1480px] m-auto bg-gradient-to-r mt-[75px] rounded-[50px]  from-purple-600 to-pink-500 text-white ">
+    <div  className="px-[15px] pb-[20px] pt-[50px] max-w-[1480px] m-auto bg-gradient-to-r mt-[75px] rounded-[50px]  from-purple-600 to-pink-500 text-white ">
       <div className="max-w-6xl  mx-auto flex flex-col md:flex-row items-center justify-between ">
         
       
